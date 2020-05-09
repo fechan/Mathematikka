7ece8567b401db07873b0c64b78a8663b5a30d1b
fechan <edu.fechan@gmail.com>
2020-05-09 16:45:08 -0700

    add documentation

--- src/main/java/io/github/fechan/mathematikka/QueryMathematicaTask.java
+++ src/main/java/io/github/fechan/mathematikka/QueryMathematicaTask.java
@@ -13,11 +13,19 @@ public class QueryMathematicaTask extends BukkitRunnable{
     private KernelLink mathematica;
     private String query;
 
+    /**
+     * Constructs a Mathematica query task, given a mathematica kernel and the query to evaluate 
+     * @param mathematica kernel that will evaluate the query
+     * @param query query to evaluate
+     */
     public QueryMathematicaTask(KernelLink mathematica, String query) {
         this.mathematica = mathematica;
         this.query = query;
     }
 
+    /**
+     * Queries Mathematica with the query and calls MathematicaQueryCompletedEvent when it's done
+     */
     @Override
     public void run() {
         String result = mathematica.evaluateToOutputForm(query, 0);
