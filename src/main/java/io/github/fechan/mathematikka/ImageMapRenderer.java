9fc723689e3864a8f066369e3b590ac49eef5466
fechan <edu.fechan@gmail.com>
2020-05-10 19:55:24 -0700

    document renderer

--- src/main/java/io/github/fechan/mathematikka/ImageMapRenderer.java
+++ src/main/java/io/github/fechan/mathematikka/ImageMapRenderer.java
@@ -10,10 +10,17 @@ import org.bukkit.map.MapView;
 public class ImageMapRenderer extends MapRenderer{
     private BufferedImage image;
 
+    /**
+     * Constructs a map renderer that renders an image
+     * @param image image to render
+     */
     public ImageMapRenderer(BufferedImage image) {
         this.image = image;
     }
 
+    /**
+     * Renders the image on the map
+     */
     public void render(MapView map, MapCanvas canvas, Player player) {
         canvas.drawImage(0, 0, image);
     }
